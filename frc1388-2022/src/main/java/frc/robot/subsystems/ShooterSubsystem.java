20f3390a55c300438fdc9b47428a6a62e1704601
Calvin <richardscalvin119@gmail.com>
2022-01-24 20:26:54 -0800

    added speed control ShooterSubsystem
    added methods
    * setTargetRPM
    * setRelativeTargetRPM
    * setEnabled
    * getTargetRPM
    * getRealRPM

--- frc1388-2022/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ frc1388-2022/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,6 +4,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
@@ -14,8 +15,17 @@ import frc.robot.Constants.ShooterConstants;
 public class ShooterSubsystem extends SubsystemBase {
 
   private final WPI_TalonFX m_shooterMotor;
+  // current rpm
+  private double m_rpm = 0;
+  // is the motor enabled
+  private boolean m_enabled = false;
 
   private static final int PID_IDX = 0;
+
+  // Math variables needed to convert RPM to ticks per second/ticks per
+  private final int SENSOR_CYCLES_PER_SECOND = 10;   // sensor velocity period is 100 ms
+  private final int SEC_PER_MIN = 60;
+  private final int COUNTS_PER_REV = 2048;
 
   /** Creates a new ShooterSubsystem. */
   public ShooterSubsystem(WPI_TalonFX shooterMotor) {
@@ -35,8 +45,38 @@ public class ShooterSubsystem extends SubsystemBase {
 
   }
 
+  public void setEnabled (boolean enabled) {
+    m_enabled = enabled;
+  }
+
+  public void setTargetRPM (double rpm) {
+    m_rpm = rpm;
+  }
+
+  public double getTargetRPM () {
+    return m_rpm;
+  }
+
+  public void setRelativeTargetRPM (double deltaRPM) {
+    m_rpm = m_rpm + deltaRPM;
+  }
+
+  public double getRealRPM () {
+    double rawSensorData = m_shooterMotor.getSelectedSensorVelocity();
+    double motorRPM = rawSensorData * SENSOR_CYCLES_PER_SECOND * SEC_PER_MIN / COUNTS_PER_REV;
+    return motorRPM;
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    if (m_enabled) {
+      double speed = m_rpm * COUNTS_PER_REV / SENSOR_CYCLES_PER_SECOND / SEC_PER_MIN;
+      m_shooterMotor.set(ControlMode.Velocity, speed);
+    } else {
+      m_shooterMotor.set(0);
+    }
+
   }
 }
