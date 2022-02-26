87231b8fd3c9ea839ebd8d39c5edde52abeb746e
Isabelle Bowdey <Isabellebowt@gmail.com>
2022-02-26 10:25:03 -0800

    Did log info, changed AutoLeaveTarmac to automove

--- frc1388-2022/src/main/java/frc/robot/commands/AutoMove.java
+++ frc1388-2022/src/main/java/frc/robot/commands/AutoMove.java
@@ -4,17 +4,20 @@
 
 package frc.robot.commands;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Robot;
 import frc.robot.Constants.AutoMoveConstants;
-import frc.robot.Constants.DriveTrainConstants;
 import frc.robot.subsystems.DriveTrainSubsystem;
 
-public class AutoLeaveTarmac extends CommandBase {
+public class AutoMove extends CommandBase {
 
+  private static final Logger log = LogManager.getLogger(AutoMove.class);
+  
   private DriveTrainSubsystem m_driveTrainSubsystem;
   private final double m_setPoint;
   private final double m_speed;
@@ -28,7 +31,7 @@ public class AutoLeaveTarmac extends CommandBase {
   } */
 
   /** Creates a new AutoMove. */
-  public AutoLeaveTarmac(DriveTrainSubsystem driveTrainSubsystem, double setPoint, double speed) {
+  public AutoMove(DriveTrainSubsystem driveTrainSubsystem, double setPoint, double speed) {
     m_driveTrainSubsystem = driveTrainSubsystem;
     m_setPoint = setPoint;
     m_speed = speed;
@@ -37,7 +40,7 @@ public class AutoLeaveTarmac extends CommandBase {
     addRequirements(driveTrainSubsystem);
 
     //Setting PID control tolerance
-    m_pidController.setTolerance(AutoMoveConstants.P_TOLERANCE); //FIXME magic number
+    m_pidController.setTolerance(AutoMoveConstants.P_TOLERANCE); //change P tolerance?
   }
 
   // Called when the command is initially scheduled.
@@ -50,16 +53,15 @@ public class AutoLeaveTarmac extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double speed = m_speed;
-    double setPoint = m_setPoint;
+    double speed;
     double leftEncoderDistance = m_driveTrainSubsystem.getLeftEncoderDistance();
 
-    speed = m_pidController.calculate(leftEncoderDistance, setPoint);
+    speed = m_pidController.calculate(leftEncoderDistance, m_setPoint);
     speed = MathUtil.clamp(speed, -m_speed, m_speed);
 
     m_driveTrainSubsystem.curvatureDrive(speed, speed, false);
 
-    //Robot.log.info(); TODO need to change log visibility to public
+    log.info("Distance: {} \tspeed: {} \tsetPoint: {}", leftEncoderDistance, speed, m_setPoint); 
   
   }
 
